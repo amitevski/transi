f81f07265f1f5d8ff4af0c08a1e633191c06ede6
Thomas Waldecker <thomas.waldecker@googlemail.com>
2011-11-17 20:30:32 +0100

    design und emit

--- app/db/views/toprated/map.js
+++ app/db/views/toprated/map.js
@@ -1,15 +1,17 @@
 //only return top rated translations
 function(doc) {
+    emit(doc.word, null);
     for (var i in doc.translations) {
-    var highest = '0';
+        var highest = '0';
         for (var j in doc.translations[i]) {
-            if ('0' === highest || 
+            if ('0' === highest ||
                 doc.translations[i][highest].rating < doc.translations[i][j].rating)
-		    {
-            	highest = j;
-		    }
-	    }
+                    {
+                highest = j;
+                    }
+            }
         emit(doc.word, {"_id": doc.translations[i][highest]._id, "rating":doc.translations[i][highest].rating});
         highest = '0';
     }
 }
+
